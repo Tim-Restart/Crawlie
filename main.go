0ea549e5e2b0066acb4b08e4c17bbefb563826fc
Tim-Restart <tim_restart@proton.me>
2025-10-15 14:13:45 +1000

    gui progress commit

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"net/url"
-	"os"
 	"sync"
-	"strconv"
+
 	//"time"
 	//"github.com/briandowns/spinner"
 	//"image/color"
@@ -13,15 +12,16 @@ import (
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
 // Layout for the GUI - Currently setup for IPloc, to be changed to Crawlie specs
+//
+// inputDomain, inputConcurrency, inputDelay, crawlButton, phoneNumberLabel, emailLabel, internalLinksLabel, externalLinksLabel
 
-func desktopLayout(inputDomain, inputConcurrency, inputDelay *widget.Entry, button, helpButton *widget.Button, fixedContainer, fixedLabels *fyne.Container, rect *canvas.Rectangle) *fyne.Container {
+func desktopLayout(inputDomain, inputConcurrency, inputDelay *widget.Entry, crawlButton *widget.Button, phoneNumberLabel, emailLabel, internalLinksLabel, externalLinksLabel *widget.Label) *fyne.Container {
 	return container.NewGridWithRows(8, // 8 rows for results/input
 		container.NewGridWithColumns(3, // 3 x 3 grid made
 			layout.NewSpacer(), // First spacer on left, first column first row
@@ -30,7 +30,7 @@ func desktopLayout(inputDomain, inputConcurrency, inputDelay *widget.Entry, butt
 				inputConcurrency,
 				inputDelay,
 			),
-			layout.NewSpacer(), // Third Column in first row
+			crawlButton, // Third Column in first row
 		),
 		container.NewVBox(
 			fixedLabels,
@@ -50,7 +50,7 @@ type config struct {
 	concurrencyControl chan struct{}
 	wg                 *sync.WaitGroup
 	maxPages           int
-	delayRequest	   int
+	delayRequest       int
 }
 
 func main() {
@@ -65,22 +65,22 @@ func main() {
 
 	inputDomain := widget.NewEntry()
 	inputDomain.SetPlaceHolder("Enter Website to Crawl")
-	inputDomain.Resize(fyne.NewSize(100,20))
+	inputDomain.Resize(fyne.NewSize(100, 20))
 
 	inputConcurrency := widget.NewEntry()
 	inputConcurrency.SetPlaceHolder("Enter how many threads")
-	inputConcurrency.Resize(fyne.NewSize(50,20))
+	inputConcurrency.Resize(fyne.NewSize(50, 20))
 
 	inputDelay := widget.NewEntry()
 	inputDelay.SetPlaceHolder("Enter request delay")
-	inputDelay.Resize(fyne.NewSize(50,20))
-	
+	inputDelay.Resize(fyne.NewSize(50, 20))
 
-	var domain []string
+	var website string
 
-	button := widget.NewButton("Crawl", func() {
+	crawlButton := widget.NewButton("Crawl", func() {
 		table.Refresh()
-		domain = checkInput(input.Text)
+		website = checkInput(input.Text)
+
 	})
 
 	var maxConcurrency int
@@ -90,60 +90,28 @@ func main() {
 	var website string
 	delayReqs := 0 // Delay requests default to 0, set in case 3 if required, and done in seconds
 
-
 	// CLI input code - left for reference whilst building GUI
 
 	/*
-
-	switch len(os.Args) {
-	case 1:
-		fmt.Println("no website provided")
-		os.Exit(1)
-	case 2:
-		fmt.Printf("starting crawl of: %v\n", os.Args[1])
-		website = os.Args[1]
-		fmt.Println("Max concurrency set to default 5")
-		maxConcurrency = 5
-	//	maxPagesSet = 10
-	case 3:
-		fmt.Printf("starting crawl of: %v\n", os.Args[1])
-		website = os.Args[1]
-		maxConcurrency, _ = strconv.Atoi(os.Args[2])
-		fmt.Printf("Max Concurrency set to : %v\n", maxConcurrency)
-	//	fmt.Println("Max pages set to default 10")
-	//	maxPagesSet = 10
-	case 4:
-		fmt.Printf("starting crawl of: %v\n", os.Args[1])
-		website = os.Args[1]
-		maxConcurrency, _ = strconv.Atoi(os.Args[2])
-		delayReqs, _ = strconv.Atoi(os.Args[3])
-		fmt.Printf("Max Concurrency set to : %v\nDelay Request set to: %v", maxConcurrency, delayReqs)
-	//	maxPagesSet, _ = strconv.Atoi(os.Args[3])
-	//	fmt.Printf("Max pages set to : %v\n", maxPagesSet)
-	default:
-		fmt.Println("Failed to set right parameters")
-		return
-	}
-
-	//fmt.Println("exited switch Statment")
-	baseURLParsed, err := stringToURL(website)
-	if err != nil {
-		fmt.Println("Failed to parse Base URL")
-		return
-	}
-
+			fmt.Printf("starting crawl of: %v\n", os.Args[1])
+			website = os.Args[1]
+			maxConcurrency, _ = strconv.Atoi(os.Args[2])
+			delayReqs, _ = strconv.Atoi(os.Args[3])
+			fmt.Printf("Max Concurrency set to : %v\nDelay Request set to: %v", maxConcurrency, delayReqs)
+		//	maxPagesSet, _ = strconv.Atoi(os.Args[3])
+		//	fmt.Printf("Max pages set to : %v\n", maxPagesSet)
 	*/
 
 	// Pickup here with the Mu and channels stuff
 	cfg := &config{
-		pages:    make(map[string]int),
-		external: make(map[string]int),
-		email:    make(map[string]int),
-		phone:    make(map[string]int),
-		baseURL:  baseURLParsed,
-		mu:      &sync.Mutex{},
+		pages:              make(map[string]int),
+		external:           make(map[string]int),
+		email:              make(map[string]int),
+		phone:              make(map[string]int),
+		baseURL:            baseURLParsed,
+		mu:                 &sync.Mutex{},
 		concurrencyControl: make(chan struct{}, maxConcurrency),
-		wg: &sync.WaitGroup{},
+		wg:                 &sync.WaitGroup{},
 		//maxPages:           maxPagesSet,
 		delayRequest: delayReqs,
 	}
@@ -170,7 +138,6 @@ func main() {
 
 	cfg.mu.Unlock()
 
-
 	// Layout for GUI defined here - likely can move to seperate package, but start from here
 
 	phoneNumberLabel := widget.NewLabel("Phone Numbers")
@@ -185,13 +152,12 @@ func main() {
 	internalLinksLabel.Resize(fyne.NewSize(80, 20))
 	internalLinksLabel.Move(fyne.NewPos(10, 0))
 
-	ExternalLinksLabel := widget.NewLabel("External Links")
-	ExternalLinksLabel.Resize(fyne.NewSize(80, 20))
-	ExternalLinksLabel.Move(fyne.NewPos(10, 0))
+	externalLinksLabel := widget.NewLabel("external Links")
+	externalLinksLabel.Resize(fyne.NewSize(80, 20))
+	externalLinksLabel.Move(fyne.NewPos(10, 0))
 
-
-
-
+	myWindow.SetContent(desktopLayout(inputDomain, inputConcurrency, inputDelay, crawlButton, phoneNumberLabel, emailLabel, internalLinksLabel, externalLinksLabel))
+	myWindow.ShowAndRun()
 
 	// return - Likely not needed now as is gui
 
